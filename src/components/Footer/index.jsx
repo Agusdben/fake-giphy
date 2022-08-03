7153830f85376fdfa2f67dd01d762f617aabeb02
Agustin Di Benedetto <agustindibenedetto98@gmail.com>
2022-08-03 06:14:43 -0300

    improved footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,25 @@
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import './Footer.css'
+
+const URLS = {
+  github: 'https://github.com/Agusdben/giphy-searchs',
+  linkedin: 'https://www.linkedin.com/in/agustin-di-benedetto-7509071ba/'
+}
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p>Created by <span>Agustin Di Benedetto</span></p>
+      <section className='footer__section'>
+        <article className='footer__links'>
+          <a className='footer__link' target='_BLANK' href={URLS.github} rel='noreferrer'><FontAwesomeIcon icon={faSquareGithub} /></a>
+          <a className='footer__link' target='_BLANK' href={URLS.linkedin} rel='noreferrer'><FontAwesomeIcon icon={faLinkedin} /></a>
+        </article>
+        <article className='footer__personal'>
+          <p className='footer__copyright'>Created by <span>Agustin Di Benedetto</span></p>
+        </article>
+      </section>
     </footer>
   )
 }
--- src/components/ReturnToTop/index.jsx
+++ src/components/ReturnToTop/index.jsx
@@ -1,18 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { useLocation } from 'react-router-dom'
+
 import './ReturnToTop.css'
 
 export const ReturnToTop = () => {
+  const scrollToTopRef = useRef()
   const location = useLocation()
   const returnToTop = (options) => window.scrollTo({ top: 0, left: 0, ...options })
+
+  useEffect(() => {
+    const scrollMaxY = (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    window.addEventListener('scroll', () => {
+      console.log({ estoy: window.scrollY, quiero: scrollMaxY })
+      if (window.scrollY === scrollMaxY && location) {
+        scrollToTopRef.current.classList.add('return-to-top--bottom')
+      } else {
+        scrollToTopRef.current.classList.remove('return-to-top--bottom')
+      }
+    })
+  }, [])
+
   useEffect(() => {
     returnToTop()
   }, [location])
 
   return (
-    <button className='return-to-top grid-center' onClick={() => returnToTop({ behavior: 'smooth' })}>
+    <button ref={scrollToTopRef} className='return-to-top grid-center' onClick={() => returnToTop({ behavior: 'smooth' })}>
       <FontAwesomeIcon icon={faArrowUp} />
     </button>
   )
